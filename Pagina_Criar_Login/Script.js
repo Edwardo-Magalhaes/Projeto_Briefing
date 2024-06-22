2dfee3dbd21a5331aa0dc1492277bbabe93e63c0
ElyssonRodrigo <162244314+ElyssonRodrigo@users.noreply.github.com>
2024-06-21 21:07:41 -0400

    alt 01

--- Pagina_Criar_Login/Script.js
+++ Pagina_Criar_Login/Script.js
@@ -1,11 +1,18 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register'); // Certifique-se de usar o id correto aqui
-const loginBtn = document.getElementById('login');
+window.onload = function() {
+    // Seleciona os elementos necessários
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
+    // Adiciona a classe "active" ao container para mostrar a parte de registro
     container.classList.add("active");
-});
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+    // Event listener para alternar entre os formulários de login e registro
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+};
