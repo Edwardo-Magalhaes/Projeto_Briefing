736812437066e01d5c1e80d5d8e8c9298ac91044
ElyssonRodrigo <162244314+ElyssonRodrigo@users.noreply.github.com>
2024-06-24 13:34:17 -0300

    alr 01

--- Pagina_Apresentação/Script.js
+++ Pagina_Apresentação/Script.js
@@ -1,9 +1,23 @@
-let ul = document.querySelector('nav .ul')
+let ul = document.querySelector('nav .ul');
+let menuItems = document.querySelectorAll('nav .ul a'); // Seleciona todos os itens da lista
 
-function openMenu(){
+function openMenu() {
     ul.classList.add('open');
-
 }
-function closeMenu(){
+
+function closeMenu() {
     ul.classList.remove('open');
+}
+
+// Adiciona um event listener a cada item da lista para fechar o menu ao ser clicado
+menuItems.forEach(item => {
+    item.addEventListener('click', closeMenu);
+});
+
+function scrollToSobreNos() {
+    document.querySelector('.apresentando').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSection() {
+    document.querySelector('.login .container').scrollIntoView({ behavior: 'smooth' });
 }
